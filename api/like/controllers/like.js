2de8127b36fb95cbe0dd856593bf22cef9cb4814
Mateo Galic <mateogalic112@gmail.com>
2021-05-16 21:48:40 +0200

    Removing like from post.

--- api/like/controllers/like.js
+++ api/like/controllers/like.js
@@ -1,8 +1,27 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+	/**
+	 * Delete like from post.
+	 *
+	 * @return {Object}
+	 */
+
+	async delete(ctx) {
+		const { user } = ctx.state;
+		const { postId } = ctx.params;
+
+		const entity = await strapi.services.like.delete({
+			post: postId,
+			user: user.id,
+		});
+
+		return sanitizeEntity(entity[0], { model: strapi.models.like });
+	},
+};
