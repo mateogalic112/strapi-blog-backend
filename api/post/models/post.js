8829cc4efb4441e996c585f35cb97e92fc543e70
Mateo Galic <mateogalic112@gmail.com>
2021-05-19 14:46:29 +0200

    Delete image after article is deleted.

--- api/post/models/post.js
+++ api/post/models/post.js
@@ -1,8 +1,24 @@
 'use strict';
 
+const { default: createStrapi } = require('strapi');
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+	lifecycles: {
+		async afterDelete(result, _) {
+			try {
+				const file = await createStrapi.plugins['upload'].services.upload.fetch({
+					id: result.featured_image.id,
+				});
+
+				await strapi.plugins['upload'].services.upload.remove(file);
+			} catch (err) {
+				console.error(err);
+			}
+		},
+	},
+};
